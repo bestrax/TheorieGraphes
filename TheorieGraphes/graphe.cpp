492e533e5e6cace0dcf43ed9fb7ef2957a8c8644
Bastien ROBERT <bastien.robert@sepefrei.fr>
2016-12-12 16:57:26 +0100

    Vertex et Arc

--- TheorieGraphes/graphe.cpp
+++ TheorieGraphes/graphe.cpp
@@ -41,32 +41,68 @@ void Graphe::load(string const filepath) {
     for (int i = 0; i< nbVertex; i++) {
         file >> name;
         file >> cost;
-        cout<<name<<" "<<cost<<endl;
-        //this->addVertex(name, cost);
+        if ( this->addVertex(name, cost) == false) {
+            cout<<"Mauvaise formation du fichier : multiple declaration du meme sommet"<<endl<<endl;
+            return;
+        }
     }
     
     while (!file.eof()) {
         file >> line;
-        cout<<line<<" "<<line.size()<<endl;
         
         if (line.size() > 2) {
             origin = line[0];
             for (int i = 1; i<line.size(); i++) {
                 if(line[i] == '.')
                     break;
-                //this->addArc(origin, line[i]);
+                if ( this->addArc(origin, line[i]) == false) {
+                    cout<<"Mauvaise formation du fichier : mauvaise contrainte"<<endl<<endl;
+                    return;
+                }
                 cout<<origin<< " " << line[i]<<endl;
             }
         }
     }
     
-    
-    
+    cout<<"a";
 }
 
-
-bool Graphe :: addVertex(char name, int cost) {
-
-
+bool Graphe::addVertex(char name, int cost) {
+    
+    if (find(vertex.begin(), vertex.end(), name) != vertex.end())
+        return false;
+    
+    this->vertex.push_back(name);
+    this->cost.push_back(cost);
+    
     return true;
 }
+
+bool Graphe::addArc(char origin, char destination) {
+    
+    if (this->adjacent.size() == 0) {
+        for(int i = 0; i < this->vertex.size(); i++) {
+            this->adjacent.push_back(vector< bool > (this->vertex.size(), false));
+        }
+    }
+    
+    if (this->values.size() == 0) {
+        for(int i = 0; i < this->vertex.size(); i++) {
+            this->values.push_back(vector< int > (this->vertex.size(), 0));
+        }
+    }
+    
+    int posOrigin = find(this->vertex.begin(), this->vertex.end(), origin) - this->vertex.begin();
+    int posDestination = find(this->vertex.begin(), this->vertex.end(), destination) - this->vertex.begin();
+    
+    if(posOrigin < 0 || posDestination < 0)
+        return false;
+    
+    this->adjacent[posOrigin][posDestination] = true;
+    this->values[posOrigin][posDestination] += this->cost[posOrigin];
+    
+    
+    return true;
+}
+
+
