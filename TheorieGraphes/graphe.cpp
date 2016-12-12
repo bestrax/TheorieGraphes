f6c7c2241da5b8f9cba4b7e73223592d76b53af2
Bastien ROBERT <bastien.robert@sepefrei.fr>
2016-12-12 17:04:07 +0100

    Merge failed

--- TheorieGraphes/graphe.cpp
+++ TheorieGraphes/graphe.cpp
@@ -23,44 +23,6 @@ Graphe::Graphe(string filepath) {
     this->load(filepath);
 }
 
-<<<<<<< HEAD
-void Graphe::load(string filepath){
-
-}
-
-
-void createMatrix (){
-    int numberVertex = this->vertex.size() + 2 ;
-    int i = 0,j = 0;
-
- // create table
-    for (i = 0;i < numberVertex; i++)
-    {
-        this->fmatrix.push_back(vector< char > (numberVertex));
-    }
-
-// initalize fmatrix
-    for (i = 0;i < numberVertex;i ++)
-    {
-        for(j = 0;j < numberVertex;j ++)
-        {
-            if ((this-> adjacent[i][j] == 1) || i == 0 || j == 0)
-                this->fmatrix[i][j] = this->values[i][j];
-            else
-                this->fmatric[i][j] = ' ';
-        }
-    }
-
-// print fmatrix
-    for(i = 0;i < numberVertex;i ++)
-    {
-        for(j = 0;j < numberVertex;j ++)
-        {
-            std :: cout << this->fmatrix[i][j] << "|";
-        }
-        std :: cout << "\n" ;
-    }
-=======
 void Graphe::load(string const filepath) {
     ifstream file(filepath);
     int nbVertex;
@@ -140,7 +102,38 @@ bool Graphe::addArc(char origin, char destination) {
     
     
     return true;
->>>>>>> b4c23b57fa8f0b12eaedcdade466493d8b4b6885
 }
 
+void Graphe::createMatrix (){
+    int numberVertex = this->vertex.size() + 2 ;
+    int i = 0,j = 0;
+    
+    // create table
+    for (i = 0;i < numberVertex; i++)
+    {
+        this->fmatrix.push_back(vector< string > (numberVertex));
+    }
+    
+    // initalize fmatrix
+    for (i = 0;i < numberVertex;i ++)
+    {
+        for(j = 0;j < numberVertex;j ++)
+        {
+            if ((this-> adjacent[i][j] == 1) || i == 0 || j == 0)
+                this->fmatrix[i][j] = this->values[i][j];
+            else
+                this->fmatrix[i][j] = "";
+        }
+    }
+    
+    // print fmatrix
+    for(i = 0;i < numberVertex;i ++)
+    {
+        for(j = 0;j < numberVertex;j ++)
+        {
+            std :: cout << this->fmatrix[i][j] << "|";
+        }
+        std :: cout << "\n" ;
+    }
+}
 
